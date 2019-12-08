560ea90697126935e12175b157a085f8037e0373
viktor <kosandyak1996@gmail.com>
2019-12-08 20:06:57 +0200

    test index_readability_service for different languages

--- spec/services/index_readability_service_spec.rb
+++ spec/services/index_readability_service_spec.rb
@@ -21,12 +21,26 @@ RSpec.describe IndexReadabilityService, type: :service do
       expect(readability_index).to eq(0)
     end
   end
+
+  describe 'when we pass valid ukraine text' do
+    let(:content) { 'Однією з родзинок української мови є те, що вона багата на зменшувальні форми. 
+      Навіть слово вороги має зменшувально-пестливу форму, яка вживається в гімні України. 
+      Пам’ятаєте: .згинуть наші вороженьки, як роса на сонці.' }
+    let(:readability_index) { IndexReadabilityService.new(content).call }
+
+    it 'return readability index 0' do
+      expect(readability_index).to eq(71)
+    end
+  end
+
+  describe 'when we pass valid russian text' do
+    let(:content) { 'Премьера продолжения сериала \"Екатерина 3. Самозванцы\" 2019 года в главной роли
+       Марина Александрова. В третьей части показан самый сложный период правления Екатерины Великой,
+       затрагивающий 1774-1776 годы.' }
+    let(:readability_index) { IndexReadabilityService.new(content).call }
+
+    it 'return readability index 0' do
+      expect(readability_index).to eq(55)
+    end
+  end
 end
-# Однією з родзинок української мови є те, що вона багата на зменшувальні форми. Навіть слово вороги має зменшувально-пестливу форму, яка вживається в гімні України. Пам’ятаєте: .згинуть наші вороженьки, як роса на сонці.
-# 71
-
-# PostgreSQL allows columns of a table to be defined as variable-length multidimensional arrays.
-# 24
-
-# Премьера продолжения сериала \"Екатерина 3. Самозванцы\" 2019 года в главной роли Марина Александрова. В третьей части показан самый сложный период правления Екатерины Великой, затрагивающий 1774-1776 годы.
-# 55
